2da615831dc3e23bf67ac75ac4f8224cfd0dd7a3
mduon2 <triet_duong@LAPTOP-0K7OMSH2.localdomain>
2025-11-15 15:49:40 -0500

    final reversed binary to ascii and decrypted code conversion doesn't work yet for some reason; need to fix

--- message03_decryption.cpp
+++ message03_decryption.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+string decimal_to_binary(int dec) {
+    string bin = "";
+    while (dec > 0) {
+        int i = dec % 2;
+        bin += to_string(i);
+        dec /= 2;
+    }
+    reverse(bin.begin(), bin.end());
+    return bin;
+}
+
+int binary_to_decimal(string bin) {
+    int dec = 0, length = bin.length();
+    for (int i = 0; i < length; i++) {
+        if (bin[length - 1 - i] == '1') {
+            dec += pow(2, i);
+        }
+    }
+    return dec;
+}
+
 int main() {
+
+    ios_base::sync_with_stdio(false);
    
+    string code, decimal = "", binary = "", decrypted = "", binary_segment = "";
+    getline(cin, code);
+
+    for (int i = 0; i < code.length(); i++) {
+        if (code[i] == ' ') {
+            binary += decimal_to_binary(stoi(decimal)) + " ";
+            decimal = "";
+            i++;
+        }
+        decimal += code[i];
+    }
+    binary += decimal_to_binary(stoi(decimal));
+    reverse(binary.begin(), binary.end());
+    
+    char asciiChar;
+
+    //this part not working for some reason
+    for (int i = 0; i < binary.length(); i++) {
+        if (binary[i] == ' ') {
+            asciiChar = static_cast<char>(binary_to_decimal(binary_segment) + '0');
+            decrypted += asciiChar;
+            binary_segment = "";
+            i++;
+        }
+        binary_segment += binary[i];
+    }
+    asciiChar = static_cast<char>(binary_to_decimal(binary_segment) + '0');
+    decrypted += char(binary_to_decimal(binary_segment));
+
+    cout<<decrypted<<endl;
+
     return 0;
 }
